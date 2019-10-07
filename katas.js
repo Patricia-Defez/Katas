e11c92531f09b25c7af196264755ac5d65ecce88
PATRICIA DEFEZ DIAZ DE SARRALDE <patricia.defez@sev.alestis>
2019-10-07 12:13:33 +0200

    07-10-2019

--- katas.js
+++ katas.js
@@ -17,3 +17,26 @@ var capitals = function (word) {
       return v === v.toUpperCase() ? memo.concat(i) : memo;
     }, []);
   };
+
+ //Sum of the first nth term of Series 07/10/2019
+ function SeriesSum(n){
+  if(n === 0) {
+    return "0.00"
+  } else {
+    let div = -2;
+    let result = 0; 
+    for (let i = 0; i < n; i++){
+    div += 3
+    result = result + (1 / (div))   
+    }
+    return result.toFixed(2).toString()
+   }
+  }
+
+  //Best
+  function SeriesSum(n) {
+    for (var s = 0, i = 0; i < n; i++) {
+      s += 1 / (1 + i * 3)
+    }  
+    return s.toFixed(2)
+  }
